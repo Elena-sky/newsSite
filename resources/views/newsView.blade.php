cbe2641d47f2f8afaadd09def678b93700e00523
MacBook <alonasoroka1993@gmail.com>
2017-12-26 20:02:24 +0200

    photo gallery don't work

--- resources/views/newsView.blade.php
+++ resources/views/newsView.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
 
+    <script>
+        $(document).ready(function () {
+            $('ul.first').bsPhotoGallery({
+                "classes": "col-lg-2 col-md-4 col-sm-3 col-xs-4 col-xxs-12",
+                "hasModal": true,
+                // "fullHeight" : false
+            });
+        });
+    </script>
     <div class="container-fluid text-center">
         <div class="row content">
             <div class="col-sm-2 sidenav">
@@ -14,7 +23,23 @@
                     <h3>{{$news->name}}</h3>
                     <div class="date">{{$news->created_at}}</div>
                     <div class="description">{{$news->description}}</div>
+                    <div>
+                        <ul class="row first">
+                            @foreach($images as $image)
+                                <li>
+                                    <img src="{{ asset("/uploads/news/$image->filename") }}" width="500px"
+                                         alt="{{$image->id}}">
+                                </li>
+                            @endforeach
+
+                        </ul>
+                        {{--<div class="col-sm-4">--}}
+                        {{--<img src="{{ asset("/uploads/news/$image->filename") }}" width="500px"--}}
+                        {{--alt="{{$image->id}}">--}}
+                        {{--</div>--}}
+                    </div>
                     <div class="category">{{$news->category_id}}</div>
+
 
                 </div>
 
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -7,6 +7,13 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="{{asset('js/photo-gallery.js')}}"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('css/jquery.bsPhotoGallery.css')}}" rel="stylesheet">
+    <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+    {{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>--}}
+    <script src="{{asset('js/jquery.bsPhotoGallery.js')}}jquery.bsPhotoGallery.js"></script>
+
     <style>
         /* Remove the navbar's default margin-bottom and rounded borders */
         .navbar {
